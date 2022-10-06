215ee2ed1c15f9da2473fbf9b25b8f6dcb2aee0a
Eometheous <jonathan.sthomas@icloud.com>
2022-10-05 17:43:19 -0700

    Added reading of maxSumTest.txt for JUnit testing.

--- src/main/java/MaxSubArrTest.java
+++ src/main/java/MaxSubArrTest.java
@@ -1,32 +1,75 @@
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class MaxSubArrTest {
-    int[] testArray = {-9, 10, -8, 10, 5, -4, -2, 5};
-    MaxSubArr maxSubArr;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-    @BeforeEach
-    public void setUp() {
-        maxSubArr = new MaxSubArr();
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MaxSubArrTest {
+    private static final int[][] testArrays = new int[10][100];
+    MaxSubArr maxSubArr;
+    private final static int[] expectedMax = new int[10];
+    private final static int[] expectedStart = new int[10];
+    private final static int[] expectedEnd = new int[10];
+    @BeforeAll
+    static void beforeAll() {
+
+        final String filePath = "/Desktop/MaxSubArrayTest/maxSumtest.txt";
+        String homePath = System.getenv("HOMEPATH");
+        if (homePath == null) homePath = System.getenv("HOME");
+        String filename = homePath + filePath;
+
+        try {
+            Scanner scannerTextFile = new Scanner(new File(filename));
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 100; j++) {
+                    testArrays[i][j] = scannerTextFile.nextInt();
+                }
+                expectedMax[i] = scannerTextFile.nextInt();
+                expectedStart[i] = scannerTextFile.nextInt();
+                expectedEnd[i] = scannerTextFile.nextInt();
+            }
+            scannerTextFile.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
     public void testBruteForce() {
-        maxSubArr.bruteForce(testArray);
-        System.out.println(maxSubArr);
+        for (int i = 0; i < 10; i++) {
+            maxSubArr = new MaxSubArr();
+            maxSubArr.bruteForce(testArrays[i]);
+            System.out.println(maxSubArr);
+            assertEquals(expectedMax[i], maxSubArr.getMax());
+            assertEquals(expectedStart[i], maxSubArr.getStart());
+            assertEquals(expectedEnd[i], maxSubArr.getEnd());
+        }
     }
 
     @Test
     public void testDivideAndConquer() {
-        maxSubArr = new MaxSubArr();
-        maxSubArr.divideAndConquer(testArray);
-        System.out.println(maxSubArr);
+        for (int i = 0; i < 10; i++) {
+            maxSubArr = new MaxSubArr();
+            maxSubArr.divideAndConquer(testArrays[i]);
+            System.out.println(maxSubArr);
+            assertEquals(expectedMax[i], maxSubArr.getMax());
+            assertEquals(expectedStart[i], maxSubArr.getStart());
+            assertEquals(expectedEnd[i], maxSubArr.getEnd());
+        }
     }
 
     @Test
     public void testKadane() {
-        maxSubArr = new MaxSubArr();
-        maxSubArr.kadane(testArray);
-        System.out.println(maxSubArr);
+        for (int i = 0; i < 10; i++) {
+            maxSubArr = new MaxSubArr();
+            maxSubArr.kadane(testArrays[i]);
+            System.out.println(maxSubArr);
+            assertEquals(expectedMax[i], maxSubArr.getMax());
+            assertEquals(expectedStart[i], maxSubArr.getStart());
+            assertEquals(expectedEnd[i], maxSubArr.getEnd());
+        }
     }
 }
