63cdc5f5f667f4ca566c154b452c225564846e6d
Eometheous <jonathan.sthomas@icloud.com>
2022-10-23 16:27:48 -0700

    Added more comments where necessary

--- src/main/java/RandomArrayUtil.java
+++ src/main/java/RandomArrayUtil.java
@@ -1,14 +1,17 @@
 import java.util.Random;
 
+/**
+ * A utility class that creates a random array of a set length and range of the numbers
+ */
 public class RandomArrayUtil {
 
     private static final Random generator = new Random();
 
-    public static int[] randomArray(int length, int size) {
+    public static int[] randomArray(int length, int range) {
         generator.setSeed(20);
         int[] a = new int[length];
         for (int i = 0; i < a.length; i++) {
-            a[i] = generator.nextInt(size) * (generator.nextBoolean() ? -1 : 1);
+            a[i] = generator.nextInt(range) * (generator.nextBoolean() ? -1 : 1);
         }
 
         return a;
